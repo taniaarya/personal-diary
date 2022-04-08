00e066e38d4d79740f2ffc61918cf433315a9f4d
Tania Arya <tania.arya@gmail.com>
2022-04-08 13:36:31 -0400

    added more unit tests to add and update

--- tests/test_diary.py
+++ tests/test_diary.py
@@ -87,7 +87,13 @@ class DiaryTestCreateEntry(unittest.TestCase):
 
     def test_valid_request_returns_nonzero_entry_id(self):
         valid_request = {"title": "Title", "body": "Body"}
-        self.assertNotEqual(self.diary.create_entry(valid_request), {"entry_id": 0})
+        self.assertNotEqual(self.diary.create_entry(valid_request), {"entry_id": "0"})
+
+    def test_response_contains_only_entry_id_key(self):
+        valid_request = {"title": "Title", "body": "Body"}
+        create_response = self.diary.create_entry(valid_request)
+        self.assertTrue("entry_id" in create_response)
+        self.assertEqual(set(create_response.keys()), {"entry_id"})
 
     def test_entry_id_is_unique(self):
         ids = []
@@ -96,15 +102,24 @@ class DiaryTestCreateEntry(unittest.TestCase):
             ids.append(self.diary.create_entry(valid_request)["entry_id"])
         self.assertEqual(len(set(ids)), 10)
 
-    def test_create_entry_db_populated_with_correct_values(self):
+    def test_single_create_entry_db_populated_with_correct_values(self):
         valid_request = {"title": "Title", "body": "Body"}
         entry_id = self.diary.create_entry(valid_request)["entry_id"]
         db_value = self.read()
-        curr_time = datetime.now()
         self.assertEqual(db_value[entry_id]["title"], "Title")
         self.assertEqual(db_value[entry_id]["body"], "Body")
         self.assertTrue(re.match(r"^[0-9]{2}/[0-9]{2}/[0-9]{4}$", db_value[entry_id]["date_created"]))
         self.assertTrue(re.match(r"^[0-9]{2}:[0-9]{2}$", db_value[entry_id]["time_created"]))
+
+    def test_multiple_create_entry_db_populated_with_correct_values(self):
+        valid_request = {"title": "Title", "body": "Body"}
+        for _ in range(10):
+            entry_id = self.diary.create_entry(valid_request)["entry_id"]
+            db_value = self.read()
+            self.assertEqual(db_value[entry_id]["title"], "Title")
+            self.assertEqual(db_value[entry_id]["body"], "Body")
+            self.assertTrue(re.match(r"^[0-9]{2}/[0-9]{2}/[0-9]{4}$", db_value[entry_id]["date_created"]))
+            self.assertTrue(re.match(r"^[0-9]{2}:[0-9]{2}$", db_value[entry_id]["time_created"]))
 
 
 class DiaryTestReadEntry(unittest.TestCase):
@@ -145,27 +160,35 @@ class DiaryTestUpdateEntry(unittest.TestCase):
 
     def setUp(self) -> None:
         self.diary = Diary()
+        fake_data = {"1": {"title": "value1", "body": "Hello World!"}, "2": {"title": "value2", "body": "Hello World!"}}
+        json_object = json.dumps(fake_data, indent=4)
+        with open(self.diary.local_db_path, "w") as outfile:
+            outfile.write(json_object)
 
     def tearDown(self) -> None:
         with open(self.diary.local_db_path, 'w') as outfile:
             outfile.write("{}")
 
-    def test_update_with_new_body_valid_entry_id_and_title_returns_updated_entry(self):
-        test_dict = {
-            20: {
-                "title": "value1",
-                "body": "Hello World!"}
-        }
-        json_object = json.dumps(test_dict, indent=4)
-        with open(self.diary.local_db_path, "w") as outfile:
-            outfile.write(json_object)
-        expected = {
-            '20': {
-                "title": "value2",
-                "body": "Hello Human!"}
-        }
-        result = self.diary.update_entry({"entry_id": '20', "title": "value2", "body": "Hello Human!"})
+    def test_single_update_with_new_body_valid_entry_id_and_title_returns_updated_entry(self):
+        expected = {"1": {"title": "value2", "body": "Hello Human!"}}
+        result = self.diary.update_entry({"entry_id": '1', "title": "value2", "body": "Hello Human!"})
         self.assertEqual(expected, result)
+
+    def test_multiple_update_to_same_entry_returns_updated_entry(self):
+        for idx in range(10):
+            new_title = "value" + str(idx)
+            new_body = "Hello Human!" + str(idx)
+            expected = {"1": {"title": new_title, "body": new_body}}
+            result = self.diary.update_entry({"entry_id": "1", "title": new_title, "body": new_body})
+            self.assertEqual(expected, result)
+
+    def test_update_to_different_entries_returns_updated_entries(self):
+        for entry_id in range(1, 3):
+            new_title = "value" + str(entry_id)
+            new_body = "Hello Human!" + str(entry_id)
+            expected = {str(entry_id): {"title": new_title, "body": new_body}}
+            result = self.diary.update_entry({"entry_id": str(entry_id), "title": new_title, "body": new_body})
+            self.assertEqual(expected, result)
 
 
 class DiaryTestIsIdInvalid(unittest.TestCase):
