e0e70aa2cb445c622052194ce74201a78d617a63
Mausam Patel <mpatel99@terpmail.umd.edu>
2022-04-17 20:19:30 -0400

    Integration tests refactor from Flask Endpoint testing to backend CRUD

--- tests/test_app_integration.py
+++ tests/test_app_integration.py
@@ -1,10 +1,9 @@
-import json
 import unittest
 from unittest import TestCase
+from personal_diary.app import Diary
 from personal_diary import db
 from personal_diary.app import create_app
 from personal_diary.models import Entry
-from personal_diary.diary import Diary
 
 
 class ApplicationTestCaseEndToEnd(TestCase):
@@ -12,70 +11,53 @@ class ApplicationTestCaseEndToEnd(TestCase):
         flask_app = create_app("test_database.db")
         flask_app.app_context().push()
         self.client = flask_app.test_client()
+        self.diary = Diary()
 
     def tearDown(self) -> None:
         db.session.query(Entry).delete()
         db.session.commit()
+        self.client = None
+        self.diary = None
 
     def test_CRUD_operations_sequentially(self):
         response_list = []
         entry_ids = []
         valid_request_json = {"title": "Title", "body": "Body"}
 
-        # test post endpoint
+        # test create entry
         for index in range(0, 50):
-            response = self.client.post("/diary", json=valid_request_json)
+            create_entry_result = self.diary.create_entry(valid_request_json)
+            entry_ids.append(create_entry_result["entry_id"])
+            response_list.append(create_entry_result)
 
-            self.assertEqual(response.status_code, 200)
+        # test read all entries
+        read_all_entries_result = self.diary.read_all_entries()
+        self.assertEqual(len(read_all_entries_result.keys()), 50)
 
-            entry_ids.append(json.loads(response.get_data())["entry_id"])
-            response_list.append(response)
-
-        # test get all endpoint
-        response_get_all_start = self.client.get("/diary")
-        self.assertEqual(response_get_all_start.status_code, 200)
-
-        response_get_all_start_data = json.loads(response_get_all_start.get_data())
-        self.assertEqual(len(response_get_all_start_data.keys()), 50)
-
-        for entry_key, entry_val in response_get_all_start_data.items():
+        for entry_key, entry_val in read_all_entries_result.items():
             self.assertEqual(entry_key in entry_ids, True)
-            self.assertEqual(set(entry_val.keys()), {"title", "body", "date_created", "time_created"})
-            self.assertEqual(response_get_all_start_data[entry_key]["title"], "Title")
-            self.assertEqual(response_get_all_start_data[entry_key]["body"], "Body")
+            self.assertEqual(read_all_entries_result[entry_key].title, "Title")
+            self.assertEqual(read_all_entries_result[entry_key].body, "Body")
 
         for entry_id in entry_ids:
-            # test get single entry endpoint
-            response_get = self.client.get(f'/diary/{entry_id}')
-            self.assertEqual(response_get.status_code, 200)
+            # test read single entry
+            read_single_entry_result = self.diary.read_single_entry({"entry_id": entry_id}).get("entry")
+            self.assertEqual(read_single_entry_result.title, "Title")
+            self.assertEqual(read_single_entry_result.body, "Body")
 
-            response_get_data = json.loads(response_get.get_data())
+            # test update entry
+            update_entry_result = self.diary.update_entry({"entry_id": entry_id, "title": "NewTitle", "body": "Edited"})
+            self.assertEqual(len(update_entry_result.keys()), 1)
+            self.assertEqual(list(update_entry_result.keys())[0], entry_id)
+            self.assertEqual(update_entry_result[entry_id].title, "NewTitle")
+            self.assertEqual(update_entry_result[entry_id].body, "Edited")
 
-            self.assertEqual(len(response_get_data.keys()), 4)
+            # test delete entry
+            delete_entry_result = self.diary.delete_entry({"entry_id": entry_id})
+            self.assertEqual(delete_entry_result["entry_id"], entry_id)
 
-            # test put endpoint
-            response_put = self.client.put("/diary", json={"entry_id": entry_id, "title": "NewTitle", "body": "Edited"})
-            self.assertEqual(response_put.status_code, 200)
-
-            response_put_data = json.loads(response_put.get_data())
-
-            self.assertEqual(len(response_put_data.keys()), 1)
-            self.assertEqual(list(response_put_data.keys())[0], entry_id)
-
-            self.assertEqual(len(list(response_put_data.values())[0]), 4)
-            self.assertEqual(response_put_data[entry_id]["title"], "NewTitle")
-            self.assertEqual(response_put_data[entry_id]["body"], "Edited")
-
-            # test delete endpoint
-            response_delete = self.client.delete("/diary", json={"entry_id": entry_id})
-            self.assertEqual(response_delete.status_code, 200)
-
-            response_delete_data = json.loads(response_delete.get_data())
-            self.assertEqual(response_delete_data["entry_id"], entry_id)
-
-        response_get_all_end = self.client.get("/diary")
-        self.assertEqual(response_get_all_end.status_code, 200)
-        self.assertEqual(len(json.loads(response_get_all_end.get_data()).keys()), 0)
+        response_get_all_end = self.diary.read_all_entries()
+        self.assertEqual(len(response_get_all_end.keys()), 0)
 
 
 if __name__ == '__main__':
